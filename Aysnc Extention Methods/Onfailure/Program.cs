bf8e11ccf50b7790da348047ac0b0009c6267cf1
Saitej Kuralla <Saitej_Kuralla@epam.com>
2023-05-12 21:59:24 +0530

    Updated to .net 6

--- Aysnc Extention Methods/Onfailure/Program.cs
+++ Aysnc Extention Methods/Onfailure/Program.cs
@@ -1,34 +1,28 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace Onfailure
+﻿internal static class Program
 {
-    internal static class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            string url = "https://example.com/data";
-            await RetrieveAsync(url).OnFailure(ex => Console.WriteLine($"Failed to retrieve data: {ex.Message}"));
-            Console.WriteLine("hello");
-        }
+        string url = "https://example.com/data";
+        await RetrieveAsync(url).OnFailure(ex => Console.WriteLine($"Failed to retrieve data: {ex.Message}"));
+        Console.WriteLine("hello");
+    }
 
-        public static async Task OnFailure(this Task task, Action<Exception> onFailure)
+    public static async Task OnFailure(this Task task, Action<Exception> onFailure)
+    {
+        try
         {
-            try
-            {
-                await task.ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                onFailure(ex);
-            }
+            await task.ConfigureAwait(false);
         }
+        catch (Exception ex)
+        {
+            onFailure(ex);
+        }
+    }
 
-        private static async Task<string> RetrieveAsync(string url)
-        {
-            await Task.Delay(3000).ConfigureAwait(false);
-            throw new Exception("Failed to retrieve data from server");
-          
-        }
+    private static async Task<string> RetrieveAsync(string url)
+    {
+        await Task.Delay(3000).ConfigureAwait(false);
+        throw new Exception("Failed to retrieve data from server");
+
     }
 }
--- Aysnc Extention Methods/Retry/Program.cs
+++ Aysnc Extention Methods/Retry/Program.cs
@@ -1,54 +1,48 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace Retry
+﻿internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            string url = "https://example.com/data";
-            int maxRetries = 3;
-            TimeSpan delayBetweenRetries = TimeSpan.FromSeconds(1);
+        string url = "https://example.com/data";
+        int maxRetries = 3;
+        TimeSpan delayBetweenRetries = TimeSpan.FromSeconds(1);
 
+        try
+        {
+            var data = await Retry(async () => await FetchDataFromServerAsync(url), maxRetries, delayBetweenRetries);
+            Console.WriteLine($"Fetched data: {data}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to fetch data after {maxRetries} attempts: {ex.Message}");
+        }
+    }
+
+
+    public static async Task<TResult?> Retry<TResult>(Func<Task<TResult>> taskFactory, int maxRetries, TimeSpan delay)
+    {
+        for (var i = 0; i < maxRetries; i++)
+        {
             try
             {
-                var data = await retry(async () => await FetchDataFromServerAsync(url), maxRetries, delayBetweenRetries);
-                Console.WriteLine($"Fetched data: {data}");
+                return await taskFactory().ConfigureAwait(false);
             }
-            catch (Exception ex)
+            catch
             {
-                Console.WriteLine($"Failed to fetch data after {maxRetries} attempts: {ex.Message}");
+                if (i == maxRetries - 1)
+                {
+                    throw;
+                }
+                await Task.Delay(delay).ConfigureAwait(false);
             }
         }
+        return default;
+    }
 
 
-        public static async Task<TResult> retry<TResult>(Func<Task<TResult>> taskFactory, int maxRetries, TimeSpan delay)
-        {
-            for (var i = 0; i < maxRetries; i++)
-            {
-                try
-                {
-                    return await taskFactory().ConfigureAwait(false);
-                }
-                catch
-                {
-                    if (i == maxRetries - 1)
-                    {
-                        throw;
-                    }
-                        await Task.Delay(delay).ConfigureAwait(false);
-                }
-            }
-            return default(TResult);
-        }
-
-
-        private static async Task<string> FetchDataFromServerAsync(string url)
-        {
-            await Task.Delay(3000).ConfigureAwait(false);
-          throw new NotImplementedException();
-            //return "data from server";
-        }
+    private static async Task<string> FetchDataFromServerAsync(string url)
+    {
+        await Task.Delay(3000).ConfigureAwait(false);
+        throw new NotImplementedException();
+        //return "data from server";
     }
 }
